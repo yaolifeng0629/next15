dcbc2021c8b9bc4e6fee2a314e33b61f9b68f667
Immerse <y15829485647@163.com>
2024-11-10 11:05:01 +0800

    feat: connect database

---
+++ prisma/migrations/20241109151145_initial/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `avatar_url` VARCHAR(191) NULL,
+    `follwers` INTEGER NOT NULL DEFAULT 0,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
